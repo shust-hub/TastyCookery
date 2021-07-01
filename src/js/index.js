5b2a2c8f47b5117e215966b0cdafae0e5b1d254a
shust-hub <alyonashustov@yandex.ru>
2021-07-01 20:01:44 +0300

    Начальная верстка

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import '../scss/index.scss'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import 'owl.carousel';
+
+  /*
+    Carousel
+*/
+$('.featureCarousel').owlCarousel({
+    items: 1
+})
